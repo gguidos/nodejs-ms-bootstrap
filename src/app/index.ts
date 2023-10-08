7c95e922291791ce961a134fec2e5137a528f170
Guido Guidos <guido.guidos@gmail.com>
2023-10-08 16:24:56 +0200

    feat(logger): add a logger

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,23 +1,26 @@
 import * as dotenv from "dotenv";
 dotenv.config()
 
+const logger = require('./libs/logger');
+
 import * as express from 'express';
-import * as compression from 'compression';
-import * as helmet from 'helmet';
+import helmet from 'helmet';
 import * as cors from 'cors';
+import * as compression from 'compression';
 
 const app = express();
 const hostname = process.env.NODE_HOSTNAME;
 const port = process.env.NODE_PORT;
 
-app.use(cors());
 app.use(helmet());
+app.use(cors());
 app.use(compression());
  
 app.get('/', (req, res) => {
+  logger.info(`[EXPRESS] path: ${req.path}, req: ${req.method}, ip: ${req.ip} `)
   res.end('Hello world!');
 })
- 
+
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  logger.info(`[EXPRESS] Server running at http://${hostname}:${port}/`);
 });
