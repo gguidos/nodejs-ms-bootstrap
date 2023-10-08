8e449781dd0be704e7d9ce7392e93beff4e995f9
Guido Guidos <guido.guidos@gmail.com>
2023-10-08 16:39:47 +0200

    reverting commit

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,26 +1,15 @@
 import * as dotenv from "dotenv";
 dotenv.config()
 
-const logger = require('./libs/logger');
-
 import * as express from 'express';
 import helmet from 'helmet';
 import * as cors from 'cors';
 import * as compression from 'compression';
 
-const app = express();
-const hostname = process.env.NODE_HOSTNAME;
-const port = process.env.NODE_PORT;
-
-app.use(cors());
-app.use(helmet());
-app.use(compression());
- 
 app.get('/', (req, res) => {
-  logger.info(`[EXPRESS] path: ${req.path}, req: ${req.method}, ip: ${req.ip} `)
   res.end('Hello world!');
 })
 
 app.listen(port, hostname, () => {
-  logger.info(`[EXPRESS] Server running at http://${hostname}:${port}/`);
+  console.log(`[EXPRESS] Server running at http://${hostname}:${port}/`);
 });
