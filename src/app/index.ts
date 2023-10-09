c07a448adac0d51216f4d56837e0444632af0a67
Guido Guidos <guido.guidos@gmail.com>
2023-10-09 11:55:58 +0200

    feat(index): configuring the server

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,14 +1,23 @@
 import * as dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
 
 import * as express from 'express';
 import helmet from 'helmet';
 import * as cors from 'cors';
 import * as compression from 'compression';
 
-app.get('/', (req, res) => {
-  res.end('Hello world!');
-})
+const app = express();
+const hostname = process.env.NODE_HOSTNAME;
+const port = process.env.NODE_PORT;
+
+app.use(helmet());
+app.options('*', cors({credentials: true, origin: true}));
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.json({ data: 'Hello, world!' }));
 
 app.listen(port, hostname, () => {
   console.log(`[EXPRESS] Server running at http://${hostname}:${port}/`);
