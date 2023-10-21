fb59997a6bc32356747f92126a03c09c23e21457
Guido Guidos <guido.guidos@gmail.com>
2023-10-21 18:31:58 +0200

    feat(test): add test

--- src/tests/use-cases/post.spec.ts
+++ src/tests/use-cases/post.spec.ts
@@ -37,7 +37,8 @@ describe('Post', () => {
       email: config.TEST_DATA.user1.email
     }
     const results = await post({ params });
-    const query = {}
+    const query = { username: params.username, email: params.email }
+    console.log(query)
     const dbContent = await dbFindDocuments({ query, ...config.DB_CONFIG });
     expect(dbContent[0]).to.have.property('username').equal(params.username);
     expect(dbContent.length).to.equal(1);
