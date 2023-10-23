ca2842dc0d98233f0662c4e9fb13d2af3fd06b21
Guido Guidos <guido.guidos@gmail.com>
2023-10-23 12:22:58 +0200

    Update post.ts

--- src/app/component/use-cases/post.ts
+++ src/app/component/use-cases/post.ts
@@ -12,32 +12,36 @@ export default function createPost({
     dbConfig,
     errorMsgs
   }){
-    let user;
-      logger.info('[POST][USE-CASE] Inserting object process - START!');
-      const userFactory = makeInputObj({ params });
-
-      user = {
-        username: userFactory.username(),
-        password: userFactory.password(),
-        email: userFactory.email(),
-        role: userFactory.role(),
-        usernameHash: userFactory.usernameHash(),
-        emailHash: userFactory.emailHash(),
-        usernamePasswordHash: userFactory.usernamePasswordHash(),
-        created: userFactory.created(),
-        modified: userFactory.modified()
+    try {
+      let user;
+        logger.info('[POST][USE-CASE] Inserting object process - START!');
+        const userFactory = makeInputObj({ params });
+  
+        user = {
+          username: userFactory.username(),
+          password: userFactory.password(),
+          email: userFactory.email(),
+          role: userFactory.role(),
+          usernameHash: userFactory.usernameHash(),
+          emailHash: userFactory.emailHash(),
+          usernamePasswordHash: userFactory.usernamePasswordHash(),
+          created: userFactory.created(),
+          modified: userFactory.modified()
+        }
+        
+        // 'or' query
+        let query = { $or: [{username: user.username}, { email: user.email }] }
+        const checkDuplicate = await findDocuments({ query, dbConfig })
+        if (checkDuplicate.length) throw new Error(errorMsgs.EXISTING_USER);
+        
+        await insertDocument({ document: user, dbConfig });
+        logger.info('[POST][USE-CASE] Inserting object process - DONE!');
+        
+        const inserted = get({ params: { username: user.username }});
+  
+        return inserted;
+      } catch (err) {
+        throw err.message;
       }
-      
-      // 'or' query
-      let query = { $or: [{username: user.username}, { email: user.email }] }
-      const checkDuplicate = await findDocuments({ query, dbConfig })
-      if (checkDuplicate.length) throw new Error(errorMsgs.EXISTING_USER);
-      
-      await insertDocument({ document: user, dbConfig });
-      logger.info('[POST][USE-CASE] Inserting object process - DONE!');
-      
-      const inserted = get({ params: { username: user.username }});
-
-      return inserted;
     }
 }
